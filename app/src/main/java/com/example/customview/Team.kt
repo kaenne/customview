0c0685e80c88ccf5361e1199dd158a7b8e8c9c05
kaenne <s_idc@mail.ru>
2025-03-27 04:27:37 +0500

    добавил модель данных

---
+++ app/src/main/java/com/example/customview/Team.kt
@@ -0,0 +1,7 @@
+package com.example.customview
+
+data class Team(
+    val name: String,      // Название команды (например, "Natus Vincere")
+    val logoName: String,  // Имя файла логотипа (например, "navi_logo")
+    val rank: Int         // Позиция в рейтинге (1, 2, 3...)
+)
