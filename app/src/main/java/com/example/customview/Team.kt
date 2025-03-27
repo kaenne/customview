47e7626282ebef2b27e5b75daafbabe684fbff7e
kaenne <s_idc@mail.ru>
2025-03-27 11:23:31 +0500

    добавил поле hltvRating для отображение рейтинга команд

--- app/src/main/java/com/example/customview/Team.kt
+++ app/src/main/java/com/example/customview/Team.kt
@@ -3,5 +3,6 @@ package com.example.customview
 data class Team(
     val name: String,      // Название команды (например, "Natus Vincere")
     val logoName: String,  // Имя файла логотипа (например, "navi_logo")
-    val rank: Int         // Позиция в рейтинге (1, 2, 3...)
+    val rank: Int,         // Позиция в рейтинге (1, 2, 3...)
+    val hltvRating: Double // рейтинг команды
 )
